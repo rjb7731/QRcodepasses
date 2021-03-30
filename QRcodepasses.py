8a8fd97127c1e51c050c6830ba42b149d58b32f9
rjb7731 <49943565+rjb7731@users.noreply.github.com>
2021-03-30 14:12:53 +0100

    Update QRcodepasses.py

--- QRcodepasses.py
+++ QRcodepasses.py
@@ -13,7 +13,6 @@ time_now = time_obj.strftime("%a-%d-%b-%y, %X")
 
 img = qrcode.make(f"Name: Joe Bloggs,  Address: 1 Downing Street DateTime:{time_now}")
 img.save("Test_sample.jpg")
-#display(img)
 
 im = cv.imread("Test_sample.jpg")
 det = cv.QRCodeDetector()
