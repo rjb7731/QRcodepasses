3a87ff3f5c447030de440b9be31ae1c10762c5cc
Ryan B <rjb7731@gmail.com>
2021-05-03 18:56:38 +0100

    "Generates QR code when form submitted"

--- QRcodepasses.py
+++ QRcodepasses.py
@@ -5,7 +5,8 @@ from PIL import Image
 import io
 import qrcode
 import json
-from flask import Flask, render_template, send_file
+import base64
+from flask import Flask, render_template, send_file, request
 
 app = Flask(__name__)
 
@@ -30,15 +31,29 @@ img = qr.make_image(fill_color="black", back_color="white")
 
 img_byte = io.BytesIO()
 img.save(img_byte, format='PNG')
-img_byte.seek(0)
 image = Image.open(img_byte)
-image_display = image.show
-# image.show()
 
 
-@ app.route('/')
-def display_qr():
-    # return send_file(img_byte, mimetype='image/PNG')
-    img_byte.seek(0)
-    # return send_file(img_byte, mimetype='image/PNG')
-    return render_template("index.html", user_image=img_byte)
+@app.route('/')
+def form():
+    return render_template('form.html')
+
+
+@app.route('/output', methods=['GET', 'POST'])
+def output():
+    if request.method == 'POST':
+        form_data = request.form
+        Arrival = f"{time_now}"
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(json.dumps(form_data))
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        img_byte = io.BytesIO()
+        img.save(img_byte, format='PNG')
+        img_byte.seek(0)
+        return send_file(img_byte, mimetype='image/png')
