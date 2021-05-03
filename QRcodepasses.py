3a40a3220259af13ddb9b8ac2c2a6c183e898c1b
Ryan B <rjb7731@gmail.com>
2021-05-03 14:54:23 +0100

    "added flask functionality"

--- QRcodepasses.py
+++ QRcodepasses.py
@@ -5,8 +5,12 @@ from PIL import Image
 import io
 import qrcode
 import json
+from flask import Flask, render_template, send_file
+
+app = Flask(__name__)
+
 time_obj = datetime.datetime.now()
-time_now = time_obj.strftime("%a-%d-%b-%y, %X")
+time_now = time_obj.strftime("%a-%d-%b-%y,%X")
 
 qr = qrcode.QRCode(
     version=1,
@@ -14,9 +18,11 @@ qr = qrcode.QRCode(
     box_size=10,
     border=4,
 )
-data = {'name': 'Joe Bloggs',
-        'mobile': "07739669145",
-        'Arrival': f"{time_now}"}
+data = {'Name': 'Joe Bloggs',
+        'Mobile': "07739669145",
+        'Arrival': f"{time_now}",
+        'Vaccinated': "1st dose"}
+
 qr.add_data(json.dumps(data))
 qr.make(fit=True)
 
@@ -24,5 +30,15 @@ img = qr.make_image(fill_color="black", back_color="white")
 
 img_byte = io.BytesIO()
 img.save(img_byte, format='PNG')
+img_byte.seek(0)
 image = Image.open(img_byte)
-image.show()
+image_display = image.show
+# image.show()
+
+
+@ app.route('/')
+def display_qr():
+    # return send_file(img_byte, mimetype='image/PNG')
+    img_byte.seek(0)
+    # return send_file(img_byte, mimetype='image/PNG')
+    return render_template("index.html", user_image=img_byte)
